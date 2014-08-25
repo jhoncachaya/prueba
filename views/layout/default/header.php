1493504e0bba5c1878880089dbeafc7e8bf89c06
ADSI04 <ADSI04@10.100.113.184>
2014-08-25 22:20:40 +0200

    Primeros archivos subidos

---
+++ views/layout/default/header.php
@@ -0,0 +1,19 @@
+
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN"
+    "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+    
+    <html xmlns="http://www.w3.org/xhtml">
+        
+        <head>
+            
+            <title><?php if (isset($this->titulo))echo $this->titulo; ?></title>
+            <meta http-equiv="Content-Type" content="text/html" charset="utf8"/>
+            <link href="<?php echo $_layoutParams ['ruta_css'].'estilos.css'; ?>" rel="stylesheet" type="text/css"/>
+                
+        </head>
+                
+        <body>
+            
+            <h1><?php echo APP_NAME; ?></h1> 
+            
+        
