42f982f62fda30cbf79cdc58411931c97a2e384a
ADSI04 <ADSI04@10.100.113.105>
2014-08-26 21:13:59 +0200

    agrego comentario

--- index.php
+++ index.php
@@ -3,6 +3,8 @@ define('DS', DIRECTORY_SEPARATOR);
 define('ROOT', realpath(dirname(__FILE__)) . DS);
 define('APP_PATH', ROOT. 'application' . DS);
 
+//se incluyen archivos
+
 require_once APP_PATH . 'Config.php';
 require_once APP_PATH . 'Request.php';
 require_once APP_PATH . 'Bootstrap.php';
